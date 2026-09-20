28ae6697f9d7c2977972f7f777041bcbec9e4c02
Ivan Ulyanchenko <python0809999@gmail.com>
2026-09-20 20:50:47 +0300

    feat: add place creation form

--- wtg/urls.py
+++ wtg/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.place_list, name="place_list"),
+    path("add/", views.place_add, name="place_add"),
     path("<int:place_id>/", views.place_detail, name="place_detail"),
 ]
--- wtg/views.py
+++ wtg/views.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
+from datetime import date
 
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .data import DEFAULT_PLACES, pick_random, with_stars
+from .forms import PlaceForm
 
 
 def get_places(request):
@@ -37,3 +39,23 @@ def home(request):
         places = get_places(request)
         place = with_stars(pick_random(places))
     return render(request, "wtg/home.html", {"place": place})
+
+
+def place_add(request):
+    if request.method == "POST":
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            places = get_places(request)
+            new_id = max((p["id"] for p in places), default=0) + 1
+            places.append(
+                {
+                    **form.cleaned_data,
+                    "id": new_id,
+                    "created": date.today().isoformat(),
+                }
+            )
+            request.session.modified = True
+            return redirect("place_list")
+    else:
+        form = PlaceForm()
+    return render(request, "wtg/place_add.html", {"form": form})
