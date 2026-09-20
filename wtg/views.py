e17470d140dadf2d7b646f17769297e5d97d92b3
Ivan Ulyanchenko <python0809999@gmail.com>
2026-09-20 17:10:43 +0300

    feat: add places list page with session storage

--- wtg/views.py
+++ wtg/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from copy import deepcopy
+
+from django.shortcuts import render
+
+from .data import DEFAULT_PLACES, rating_stars
+
+
+def get_places(request):
+    if "places" not in request.session:
+        request.session["places"] = deepcopy(DEFAULT_PLACES)
+    return request.session["places"]
+
+
+def place_list(request):
+    places = get_places(request)
+    items = [{**place, "stars": rating_stars(place["rating"])} for place in places]
+    return render(request, "wtg/place_list.html", {"places": items})
